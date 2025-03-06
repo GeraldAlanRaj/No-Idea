b5606c1cd3ef0c7bda52d1fa6f779d8effc44ae2
GeraldAlanRaj <geraldalanraj@gmail.com>
2025-03-06 15:20:36 +0530

    feat : Added the correct design and functionality to the contact page

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,11 +1,40 @@
 import Navbar from "../components/Navbar";
 import Email from "../components/Mailjs";
+import mailImage from "../assets/Mail.png";
+import locationImage from '../assets/Location.png';
+import phoneImage from '../assets/Phone.png';
+import '../styles/Contact.css';
 
 const Contact = () => { 
   return (
-    <div>
+    <div className="Contact-Container">
+      <div className="Navbar">
         <Navbar />
-        <Email />
+      </div>
+
+      <div className="Contact-Body">
+        <div className="Header">
+          <h1>Get in Touch!</h1>
+          <h2>Contact us for more assitance</h2>
+        </div>
+        <div className="Middle">
+          <div className="Mail_Id">
+            <img src={mailImage} alt="mail" />
+            <h2>noidea.fitness@gmail.com</h2>
+          </div>    
+          <div className="Phone_No">
+            <img src={phoneImage} alt="phone" />
+            <h2>123-456</h2>
+          </div>    
+          <div className="Location">
+            <img src={locationImage} alt="location" />
+            <h2>ABC,DEF</h2>
+          </div>    
+        </div>
+        <div className="Footer">
+          <Email />
+        </div>
+      </div>
     </div>
   );
 }
