98afaf68219ed30afc8ab2b6d761dd32539975ab
GeraldAlanRaj <geraldalanraj@gmail.com>
2025-03-13 13:47:59 +0530

    fix : Updated the profile page to get the user details

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "../styles/pages/Profile.css"
 
 const Profile = ({ onClose }) => {
+
+    const[age, setAge] = useState();
+    const[height, setHeight] = useState();
+    const[weight, setWeight] = useState();
+    const [gender, setGender] = useState("");
+    const[activity, setActivity] = useState("");
+
+    const handle_radio = (e) => {
+      setGender(e.target.value);
+    };
+
+    const handle_selectbox = (e) => {
+      setActivity(e.target.value);
+    };
+
     
     const navigate = useNavigate(); // Move useNavigate inside the component
 
@@ -37,6 +53,31 @@ const Profile = ({ onClose }) => {
         <button className="close-btn" onClick={onClose}>X</button>
         <h2>User Profile</h2>
         <p>Hi! {username}</p>
+
+
+        <div className="User-Details">
+        <input type="number" placeholder="Enter Age" value={age} onChange={(e) => setAge(e.target.value)} />
+        <input type="number" placeholder="Enter Height" value={height} onChange={(e) => setHeight(e.target.value)} />
+        <input type="number" placeholder="Enter Weight" value={weight} onChange={(e) => setWeight(e.target.value)} />
+        <div>
+        <label>
+          <input type="radio" value="Male" checked={gender === "Male"} onChange={handle_radio} />
+          Male
+        </label>
+        <label>
+          <input type="radio" value="Female" checked={gender === "Female"} onChange={handle_radio} />
+          Female
+        </label>
+        </div>
+        <div>
+        <select value={activity} onChange={handle_selectbox}>
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+        </div>
+        </div>
+
         <button onClick={handleLogout}>Logout</button>
       </div>
     );
