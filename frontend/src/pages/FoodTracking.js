3924856e6061be4c6ce7eca5449b66e644c3f0ff
GeraldAlanRaj <geraldalanraj@gmail.com>
2025-04-26 15:58:23 +0530

    Modified the styles for the food tracking component

--- frontend/src/pages/FoodTracking.js
+++ frontend/src/pages/FoodTracking.js
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
 import instance from "../utils/axiosInterceptor";
 import FoodDetail from "../components/food-tracking/foodDetails";
+import '../styles/components/food-tracking/foodTracking.css'
 
 const FoodTracking = () => {
   const [search, setSearch] = useState("");
@@ -20,28 +21,36 @@ const FoodTracking = () => {
     }
   };
 
-  // Match dynamic food detail route
   if (/^\/food\/[a-zA-Z0-9]+$/.test(location.pathname)) {
     return <FoodDetail />;
   }
 
   return (
-    <div className="FoodTracking-Container">
+    <div className="food-tracking-container">
       <Navbar />
-      <div className="Search-Bar">
+      <h1 className="food-tracking-title">
+            <span style={{ color: "white" }}>Search by</span>
+            <span style={{ color: "#454545" }}> Food Name</span>
+      </h1>
+      <div className="search-bar">
         <SearchBar search={search} setSearch={setSearch} handleSearch={handleSearch} />
       </div>
-      <ul>
-        {results.map((result) => (
-          <li
-            key={result._id}
-            onClick={() => navigate(`/food/${result._id}`)}
-            style={{ cursor: "pointer" }}
-          >
-            {result.name}
-          </li>
-        ))}
-      </ul>
+
+      <div className="results-list">
+        {results.length === 0 ? (
+          <p className="no-results">No results found.</p>
+        ) : (
+          results.map(result => (
+            <div
+              key={result._id}
+              className="food-item"
+              onClick={() => navigate(`/food/${result._id}`)}
+            >
+              <h3 className="food-name">{result.name}</h3>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
